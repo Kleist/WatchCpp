55649749d4bd19630b33a6c394e03f7dc1977506
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2015-06-06 21:38:13 +0200

    Reorder includes to avoid bogus warning from CLion

--- src/Watcher.cpp
+++ src/Watcher.cpp
@@ -1,27 +1,22 @@
-//
-// Created by andreas on 6/6/15.
-//
-
 #include "Watcher.h"
 
+#include <limits.h>
 #include <sys/inotify.h>
 #include <unistd.h>
-#include <limits.h>
 #include <iostream>
 
-Watcher::Watcher(const std::string& filename, std::function<void()> onChange)
-{
-   std::promise<void> threadStarted;
+Watcher::Watcher(const std::string& filename, std::function<void()> onChange)  {
+    std::promise<void> threadStarted;
     std::thread thread([this, filename, onChange, &threadStarted]() {
         run_(filename, onChange, threadStarted);
     });
     using std::swap;
     swap(thread_, thread);
-   threadStarted.get_future().get();
+    threadStarted.get_future().get();
 }
 
 void Watcher::run_(const std::string& filename, std::function<void()> onChange, std::promise<void>& watching) {
-   std::cout << "Thread running" << std::endl;
+    std::cout << "Thread running" << std::endl;
     int inotfd = inotify_init();
     /*int watch_desc = */ inotify_add_watch(inotfd, filename.c_str(), IN_MODIFY);
     size_t bufsiz = sizeof(struct inotify_event) + PATH_MAX + 1;
@@ -29,12 +24,9 @@ void Watcher::run_(const std::string& filename, std::function<void()> onChange, 
     auto event = std::unique_ptr<struct inotify_event, FreeFunction>(reinterpret_cast<struct inotify_event*>(malloc(bufsiz)), free);
 
     watching.set_value();
-    /* wait for an event to occur */
     read(inotfd, event.get(), bufsiz);
     onChange();
     std::cout << "After read" << std::endl;
-
-    /* process event struct here */
 }
 
 void Watcher::join() {
