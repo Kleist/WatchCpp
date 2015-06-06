353aee7f49a739146057f5232df9313a64d92a07
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2015-06-06 22:31:57 +0200

    Remove debug output.

--- src/Watcher.cpp
+++ src/Watcher.cpp
@@ -16,17 +16,15 @@ Watcher::Watcher(const std::string& filename, std::function<void()> onChange)  {
 }
 
 void Watcher::run_(const std::string& filename, std::function<void()> onChange, std::promise<void>& watching) {
-    std::cout << "Thread running" << std::endl;
     int inotfd = inotify_init();
     /*int watch_desc = */ inotify_add_watch(inotfd, filename.c_str(), IN_MODIFY);
     size_t bufsiz = sizeof(struct inotify_event) + PATH_MAX + 1;
     typedef decltype(free)* FreeFunction;
-    auto event = std::unique_ptr<struct inotify_event, FreeFunction>(reinterpret_cast<struct inotify_event*>(malloc(bufsiz)), free);
+    auto event = std::unique_ptr<struct inotify_event, FreeFunction>(reinterpret_cast<struct inotify_event*>(malloc(bufsiz)), &free);
 
     watching.set_value();
     read(inotfd, event.get(), bufsiz);
     onChange();
-    std::cout << "After read" << std::endl;
 }
 
 void Watcher::join() {
--- test/integration/test_watch_file.cpp
+++ test/integration/test_watch_file.cpp
@@ -8,21 +8,16 @@ using namespace std;
 
 int main() {
     const std::string filename = "touch-test";
-    std::cout << "Startup" << std::endl;
     std::atomic_bool fileTouched{false};
     std::ofstream ofs(filename);
     ofs.close();
-    std::cout << "File created" << std::endl;
     Watcher watcher(filename, [&]() {
         fileTouched = true;
     });
-    std::cout << "Watcher created" << std::endl;
     ofs.open(filename);
     ofs << "Test file";
     ofs.close();
-    std::cout << "File touched" << std::endl;
     watcher.join();
-    std::cout << "Joined" << std::endl;
     assert(fileTouched);
     return 0;
 }
